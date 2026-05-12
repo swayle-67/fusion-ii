03d82255212a69f14b36f17ebb1b4bb89f846f85
swayle-67 <omarsuhail786@gmail.com>
2026-05-12 11:04:43 +0200

    debug py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,10 @@
-from app import app
-
-app = app
+import traceback
+try:
+    from app import app
+except Exception as e:
+    import flask
+    app = flask.Flask(__name__)
+    @app.route("/")
+    @app.route("/<path:path>")
+    def error(path=""):
+        return f"<pre>{traceback.format_exc()}</pre>", 500
